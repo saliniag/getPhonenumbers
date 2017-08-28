106ce40a560bb57840d316121f294a0d9ae7d9d0
saliniag <salini.agarwal11@gmail.com>
2017-08-28 12:56:53 -0700

    getPhoneNumber

---
+++ HRmapAndDictionary/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRmapAndDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int noOfentry = Convert.ToInt32(Console.ReadLine());
+            Dictionary<string, int> phoneBook = new Dictionary<string, int>();
+            for (int i = 0; i < noOfentry; i++)
+            {
+                string []entry= Console.ReadLine().Split(' ');
+
+                phoneBook.Add(entry[0], Convert.ToInt32(entry[1]));
+
+            }
+            string input = Console.ReadLine();
+            while (input != null)
+            {
+                if (input == "")
+                {
+                    break;
+                }
+                else
+                {
+                    if (phoneBook.ContainsKey(input) && input != null)
+                    {
+                        KeyValuePair<string, int> kv = phoneBook.FirstOrDefault(k => k.Key == input);
+                        Console.WriteLine(kv.Key + "=" + kv.Value);
+
+                    }
+
+                    else { Console.WriteLine("Not found"); }
+                    input = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
